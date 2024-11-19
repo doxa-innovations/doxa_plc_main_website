620575b2bac99cb979f6e12f24b56d44f37632c1
Ephrem K. Getachew <111271251+ephi2github@users.noreply.github.com>
2024-11-19 21:01:28 +0300

    New update
    Signed-off-by: Ephrem K. Getachew <111271251+ephi2github@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
 import {Route, Routes} from "react-router";
 import {Home} from "./Pages/Home.tsx";
-import honeycone from '../src/images/SVGs/honeycone.svg'
-import honeycone2 from '../src/images/SVGs/honeycone2.svg'
-import bee1 from '../src/images/SVGs/bee1.svg'
-import bee2 from '../src/images/SVGs/bee2.svg'
-import bee3 from '../src/images/SVGs/bee3.svg'
+import honeycone from './images/SVGs/honeycone.svg'
+import honeycone2 from './images/SVGs/honeycone2.svg'
+import bee1 from './images/SVGs/bee1.svg'
+import bee2 from './images/SVGs/bee2.svg'
+import bee3 from './images/SVGs/bee3.svg'
+import logo from './images/logo.png'
 
 function App() {
     
     return (
         <main className="h-dvh w-full overflow-hidden">
-            <div className="w-full h-full bg-pj-light-Dark grid ">
-                <img className='absolute top-0 right-0 w-28 md:w-44 z-0' src={honeycone} alt=""/>
-                <img className='absolute top-16 md:top-28 right-2 w-20 md:w-36 z-0' src={bee1} alt=""/>
-                <img className="absolute top-0 left-10 md:left-48 w-20 md:w-36 z-0" src={bee2} alt=""/>
+            <div className="w-full h-full bg-pj-light-Dark grid grid-rows-12">
+                <div className={`h-full row-span-2 md:row-span-3 lg:row-span-4 grid grid-flow-col grid-cols-3`}>
+                    <div className={`flex md:justify-end items-start`}>
+                        <img className='w-8/12' src={bee2} alt=""/>
+                    </div>
+                    <div className={`grid place-items-center`}>
+                        <img className='w-10/12' src={logo} alt=""/>
+                    </div>
+                    <div className={`h-full grid justify-end relative`}>
+                        <img className='h-3/6 md:h-5/6 absolute top-12 right-14 md:right-24 lg:right-28' src={bee1} alt=""/>
+                        <img className={'h-4/6 md:h-3/4 '} src={honeycone} alt=""/>
+                    </div>
+                
+                </div>
+                
                 
                 <Routes>
-                    <Route index element={<Home />} />
+                <Route index element={<Home/>}/>
                     {/*<Route path='/services' element={<Services />} />*/}
                     {/*<Route path='/prices' element={<Prices />} />*/}
                     {/*<Route path='/team' element={<Team />} />*/}
@@ -26,8 +38,8 @@ function App() {
                     {/*<Route path='*' element={<Home />} />*/}
                 </Routes>
                 
-                <img className='absolute left-0 top-[40rem] md:top-[20rem] w-24 md:w-40 z-0' src={honeycone2} alt=""/>
-                <img className='absolute top-[44rem] md:top-[28rem] left-20 md:left-24 w-20 md:w-36 z-0' src={bee3} alt=""/>
+                <img className='w-2/12' src={honeycone2} alt=""/>
+                <img className='w-2/12' src={bee3} alt=""/>
             </div>
         </main>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
+import {StrictMode} from "react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
