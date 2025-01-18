9499e88cd69c060121584a211e71f01682deda3f
Ephrem K. Getachew <111271251+ephi2github@users.noreply.github.com>
2025-01-18 15:52:26 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+		distDir: 'build',
+		experimental: {
+				// This is experimental but can
+				// be enabled to allow parallel threads
+				// with nextjs automatic static generation
+				workerThreads: false,
+				cpus: 1
+		}
 };
 
 export default nextConfig;
