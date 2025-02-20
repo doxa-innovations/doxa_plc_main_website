f61deb20d58cac2c5964c110ef5aa4aa5c8e661a
Ephrem Getachew <ephigetachek@gmail.com>
2025-02-20 15:43:19 +0300

    Feature: Dockerized and Deployable

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+export default (phase, { defaultConfig }) => {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        /* config options here */
+        
+        output: "standalone",
+    }
+    return nextConfig
+}
